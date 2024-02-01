64115652c86b8e1cf984caf6d3b653ff426cc5a4
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-01 21:10:32 +0100

    Made it a wee-bit closer to T-Flows
    On branch main
    
    modified:   .gitignore
    new file:   Example_004/.Modules/readme
    new file:   Example_004/.Objects/readme
    new file:   Example_004/Compute_Mod.f90
    renamed:    Example_004/Compute/Mat_Mul.f90 -> Example_004/Compute_Mod/Mat_Mul.f90
    deleted:    Example_004/Example_004.f90
    new file:   Example_004/Full_Matrix_Mod.f90
    renamed:    Example_004/Full_Matrix/Allocate.f90 -> Example_004/Full_Matrix_Mod/Allocate.f90
    renamed:    Example_004/Full_Matrix/Copy_From_Device.f90 -> Example_004/Full_Matrix_Mod/Copy_From_Device.f90
    renamed:    Example_004/Full_Matrix/Copy_To_Device.f90 -> Example_004/Full_Matrix_Mod/Copy_To_Device.f90
    new file:   Example_004/Main.f90
    modified:   Example_004/compile.sh

---
+++ Example_004/Main.f90
@@ -0,0 +1,38 @@
+!==============================================================================!
+  program matrix_addition
+!------------------------------------------------------------------------------!
+  use Full_Matrix_Mod
+  use Compute_Mod
+!------------------------------------------------------------------------------!
+  implicit none
+!------------------------------------------------------------------------------!
+  integer, parameter     :: N = 10000
+  type(Full_Matrix_Type) :: A, B, C
+!==============================================================================!
+
+  ! Allocate matrices
+  call A % Full_Matrix_Allocate(N)
+  call B % Full_Matrix_Allocate(N)
+  call C % Full_Matrix_Allocate(N)
+
+  ! Initialize matrices
+  A % val(:,:) = 1.0
+  B % val(:,:) = 2.0
+  C % val(:,:) = 0.0
+
+  call A % Full_Matrix_Copy_To_Device()
+  call B % Full_Matrix_Copy_To_Device()
+  call C % Full_Matrix_Copy_To_Device()
+
+  call Global_Compute % Compute_Mat_Mul(N, A % val, B % val, C % val)
+
+  call C % Full_Matrix_Copy_From_Device()
+
+  ! Print result
+  print *, 'Matrix c(1,  1  ):', C % val(1,   1)
+  print *, 'Matrix c(2,  2  ):', C % val(2,   2)
+  print *, 'Matrix c(n-1,n-1):', C % val(N-1, N-1)
+  print *, 'Matrix c(n,  n  ):', C % val(N,   N)
+
+  end program
+
--- Example_004/compile.sh
+++ Example_004/compile.sh
@@ -1,2 +1,10 @@
-nvfortran                   -cpp Example_004.f90 -o No_Gpu
-nvfortran -acc -Minfo=accel -cpp Example_004.f90 -o With_Gpu
+nvfortran -acc -Minfo=accel -cpp -module .Modules/ -c Full_Matrix_Mod.f90
+nvfortran -acc -Minfo=accel -cpp -module .Modules/ -c Compute_Mod.f90
+nvfortran -acc -Minfo=accel -cpp -module .Modules/ -c Main.f90
+nvfortran -acc -Minfo=accel -cpp -module .Modules/ Full_Matrix_Mod.o Compute_Mod.o Main.o -o With_Gpu
+
+nvfortran                   -cpp -module .Modules/ -c Full_Matrix_Mod.f90
+nvfortran                   -cpp -module .Modules/ -c Compute_Mod.f90
+nvfortran                   -cpp -module .Modules/ -c Main.f90
+nvfortran                   -cpp -module .Modules/ Full_Matrix_Mod.o Compute_Mod.o Main.o -o No_Gpu
+
