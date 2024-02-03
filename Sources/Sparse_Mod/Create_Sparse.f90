339a6191b93bb8a075ffbdfed4acb2c1ab6e584a
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-03 13:26:32 +0100

    Simplified the formation of the sparse matrix
    On branch main
    
    modified:   Sparse_Mod/Create_Sparse.f90

--- Sources/Sparse_Mod/Create_Sparse.f90
+++ Sources/Sparse_Mod/Create_Sparse.f90
@@ -6,7 +6,7 @@
   class(Sparse_Type) :: a
   type(Grid_Type)    :: Grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: i, j, k, ni, nj, nk, pass, non_zeros
+  integer :: i, j, k, ni, nj, nk, non_zeros
   integer :: c, w, e, s, n, b, t
   integer :: col_a, col_b, row_a, row_b, pos_a, pos_b
   real    :: dx, dy, dz
@@ -22,129 +22,126 @@
   dy = Grid % ly / Grid % ny
   dz = Grid % lz / Grid % nz
 
-  !--------------------------------------------------------------------!
-  !   Make two passes; in the first one count non-zeros and allocate   !
-  !   memory, in the second one, form the compressed matrix            !
-  !--------------------------------------------------------------------!
-  do pass = 1, 2
+  !------------------------------------------!
+  !   Count non-zeroes and allocate memory   !
+  !------------------------------------------!
+  non_zeros = 0
 
-    non_zeros = 0
+  ! Browse in a way in which cell number "a" will increase one by one
+  do k = 1, nk
+    do j = 1, nj
+      do i = 1, ni
+        c = (k-1)*ni*nj + (j-1)*ni + i
 
-    ! Browse in a way in which cell number "a" will increase one by one
-    do k = 1, nk
-      do j = 1, nj
-        do i = 1, ni
-          c = (k-1)*ni*nj + (j-1)*ni + i
+        if(k > 1) non_zeros = non_zeros + 1
+        if(j > 1) non_zeros = non_zeros + 1
+        if(i > 1) non_zeros = non_zeros + 1
+        non_zeros = non_zeros + 1
+        if(i < ni) non_zeros = non_zeros + 1
+        if(j < nj) non_zeros = non_zeros + 1
+        if(k < nk) non_zeros = non_zeros + 1
 
-          ! First neighbours
-          e = c+1
-          w = c-1
-          n = c+ni
-          s = c-ni
-          t = c+ni*nj
-          b = c-ni*nj
-
-          ! If second pass, set row index
-          if(pass == 2) then
-            a % row(c) = non_zeros + 1
-          end if
-
-          !-------!
-          !   B   !
-          !-------!
-          if(k > 1) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = b
-              a % val(non_zeros) = -(dx*dy) / dz
-            end if
-          end if
-
-          !-------!
-          !   S   !
-          !-------!
-          if(j > 1) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = s
-              a % val(non_zeros) = -(dx*dz) / dy
-            end if
-          end if
-
-          !-------!
-          !   W   !
-          !-------!
-          if(i > 1) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = w
-              a % val(non_zeros) = -(dy*dz) / dx
-            end if
-          end if
-
-          !-------------!
-          !             !
-          !   Central   !
-          !             !
-          !-------------!
-          non_zeros = non_zeros + 1
-          if(pass == 2) then
-            a % col(non_zeros) = c
-            a % val(non_zeros) = 4.0 * ( dx*dy/dz + dx*dz/dy + dy*dz/dx )
-          end if
-
-          !-------!
-          !   E   !
-          !-------!
-          if(i < ni) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = e
-              a % val(non_zeros) = -(dy*dz) / dx
-            end if
-          end if
-
-          !-------!
-          !   N   !
-          !-------!
-          if(j < nj) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = n
-              a % val(non_zeros) = -(dx*dz) / dy
-            end if
-          end if
-
-          !-------!
-          !   T   !
-          !-------!
-          if(k < nk) then
-            non_zeros = non_zeros + 1
-            if(pass == 2) then
-              a % col(non_zeros) = t
-              a % val(non_zeros) = -(dx*dy) / dz
-            end if
-          end if
-
-        end do
       end do
     end do
-
-    if(pass == 2) then
-      a % row(ni*nj*nk+1) = non_zeros+1
-    end if
-
-    if(pass == 1) then
-      print *, '# Number of nonzeros: ', non_zeros
-      a % n        = ni*nj*nk
-      a % nonzeros = non_zeros
-      allocate (a % row(ni*nj*nk+1)); a % row = 0
-      allocate (a % dia(ni*nj*nk));   a % dia = 0
-      allocate (a % col(non_zeros));  a % col = 0
-      allocate (a % val(non_zeros));  a % val = 0
-      allocate (a % mir(non_zeros));  a % mir = 0
-    end if
   end do
+
+  print *, '# Number of nonzeros: ', non_zeros
+  a % n        = ni*nj*nk
+  a % nonzeros = non_zeros
+  allocate (a % row(ni*nj*nk+1)); a % row = 0
+  allocate (a % dia(ni*nj*nk));   a % dia = 0
+  allocate (a % col(non_zeros));  a % col = 0
+  allocate (a % val(non_zeros));  a % val = 0
+  allocate (a % mir(non_zeros));  a % mir = 0
+
+  !--------------------------------!
+  !   Form the compressed matrix   !
+  !--------------------------------!
+  non_zeros = 0
+
+  ! Browse in a way in which cell number "a" will increase one by one
+  do k = 1, nk
+    do j = 1, nj
+      do i = 1, ni
+        c = (k-1)*ni*nj + (j-1)*ni + i
+
+        ! First neighbours
+        e = c+1
+        w = c-1
+        n = c+ni
+        s = c-ni
+        t = c+ni*nj
+        b = c-ni*nj
+
+        ! If second pass, set row index
+        a % row(c) = non_zeros + 1
+
+        !-------!
+        !   B   !
+        !-------!
+        if(k > 1) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = b
+          a % val(non_zeros) = -(dx*dy) / dz
+        end if
+
+        !-------!
+        !   S   !
+        !-------!
+        if(j > 1) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = s
+          a % val(non_zeros) = -(dx*dz) / dy
+        end if
+
+        !-------!
+        !   W   !
+        !-------!
+        if(i > 1) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = w
+          a % val(non_zeros) = -(dy*dz) / dx
+        end if
+
+        !-------!
+        !   C   !
+        !-------!
+        non_zeros = non_zeros + 1
+        a % col(non_zeros) = c
+        a % val(non_zeros) = 4.0 * ( dx*dy/dz + dx*dz/dy + dy*dz/dx )
+
+        !-------!
+        !   E   !
+        !-------!
+        if(i < ni) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = e
+          a % val(non_zeros) = -(dy*dz) / dx
+        end if
+
+        !-------!
+        !   N   !
+        !-------!
+        if(j < nj) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = n
+          a % val(non_zeros) = -(dx*dz) / dy
+        end if
+
+        !-------!
+        !   T   !
+        !-------!
+        if(k < nk) then
+          non_zeros = non_zeros + 1
+          a % col(non_zeros) = t
+          a % val(non_zeros) = -(dx*dy) / dz
+        end if
+
+      end do
+    end do
+  end do
+
+  a % row(ni*nj*nk+1) = non_zeros+1
 
   !---------------------------------!
   !   Find positions of diagonals   !
