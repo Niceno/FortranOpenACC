c2fe891b57fc835ef4d233e2efc858960c9bd14b
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-19 19:34:08 +0100

    Coppied Grid from Visual Fortran Solvers
    ... and Assert class and related files along.
    
    new file:   Assert.h90
    new file:   Assert_Mod.f90
    new file:   Assert_Mod/Handle_Assert.f90
    modified:   Grid_Mod.f90
    new file:   Grid_Mod/Load_Grid.f90
    modified:   makefile
    modified:   makefile_explicit_dependencies

--- Sources/Grid_Mod.f90
+++ Sources/Grid_Mod.f90
@@ -1,10 +1,39 @@
+#include "Assert.h90"
+
 !==============================================================================!
   module Grid_Mod
+!------------------------------------------------------------------------------!
+  use Assert_Mod
 !------------------------------------------------------------------------------!
   implicit none
 !------------------------------------------------------------------------------!
 !   Defines a (ludicrously simple) grid                                        !
 !==============================================================================!
+
+  !-----------------------------!
+  !                             !
+  !   Boundary condition type   !
+  !                             !
+  !-----------------------------!
+  type Bc_Type
+
+    ! Types on all sides (N will be for Neumann, D for Dirichlet)
+    character(1) :: west_t
+    character(1) :: east_t
+    character(1) :: south_t
+    character(1) :: north_t
+    character(1) :: bottom_t
+    character(1) :: top_t
+
+    ! Values on all sides
+    real :: west_v
+    real :: east_v
+    real :: south_v
+    real :: north_v
+    real :: bottom_v
+    real :: top_v
+
+  end type
 
   !---------------!
   !               !
@@ -23,10 +52,13 @@
     real    :: lx, ly, lz  ! domain size in x, y and z direction
     real    :: dx, dy, dz  ! cell size in x, y and z direction
 
+    type(Bc_Type) :: bc  ! boundary conditions
+
     contains
       procedure :: Create_Grid
       procedure :: Cell_Number
       procedure :: Cells_I_J_K
+      procedure :: Load_Grid
       procedure :: Save_Vtk_Debug
 
   end type
@@ -35,6 +67,7 @@
 #   include "Grid_Mod/Create_Grid.f90"
 #   include "Grid_Mod/Cell_Number.f90"
 #   include "Grid_Mod/Cells_I_J_K.f90"
+#   include "Grid_Mod/Load_Grid.f90"
 #   include "Grid_Mod/Save_Vtk_Debug.f90"
 
   end module
