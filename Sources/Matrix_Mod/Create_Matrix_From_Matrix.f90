3638a7dad6525fe9341922ab97eea44d8f5c5c54
Niceno <bojan.niceno.scientist@gmail.com>
2024-02-24 18:53:32 +0100

    Little fixes, some cases did not work
    On branch main
    
    modified:   Matrix_Mod/Create_Matrix.f90
    modified:   Matrix_Mod/Create_Matrix_From_Matrix.f90
    modified:   test_004_cube.ini

--- Sources/Matrix_Mod/Create_Matrix_From_Matrix.f90
+++ Sources/Matrix_Mod/Create_Matrix_From_Matrix.f90
@@ -19,10 +19,12 @@
 
   ! Copy all significant fields from the matrix
   A % nonzeros = B % nonzeros
-  allocate (A % row(Grid % n_cells+1));  A % row(:) = B % row(:)
-  allocate (A % dia(Grid % n_cells));    A % dia(:) = B % dia(:)
-  allocate (A % col(A % nonzeros));      A % col(:) = B % col(:)
-  allocate (A % val(A % nonzeros));      A % val(:) = B % val(:)
-  allocate (A % mir(A % nonzeros));      A % mir(:) = B % mir(:)
+  allocate (A % val(A % nonzeros));       A % val(:)   = B % val(:)
+  allocate (A % col(A % nonzeros));       A % col(:)   = B % col(:)
+  allocate (A % row(Grid % n_cells+1));   A % row(:)   = B % row(:)
+  allocate (A % dia(Grid % n_cells));     A % dia(:)   = B % dia(:)
+  allocate (A % mir(A % nonzeros));       A % mir(:)   = B % mir(:)
+  allocate (A % pos(2, Grid % n_faces));  A % pos(:,:) = B % pos(:,:)
+  allocate (A % d_inv(Grid % n_cells));   A % d_inv(:) = B % d_inv(:)
 
   end subroutine
