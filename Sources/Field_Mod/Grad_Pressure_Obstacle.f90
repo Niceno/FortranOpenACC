4ce0ed74d8a35df4ffe13a06f4902b4f4862ad47
Niceno <bojan.niceno.scientist@gmail.com>
2024-03-01 22:13:23 +0100

    Much shorter version which perform the same as old
    I managed to abstract all coorindate direcion into one,
    abstract dimension.  The code is not only faster this way,
    it also avoids code duplication.
    
    On branch main
    
    modified:   Field_Mod/Grad_Pressure_Obstacle.f90

--- Sources/Field_Mod/Grad_Pressure_Obstacle.f90
+++ Sources/Field_Mod/Grad_Pressure_Obstacle.f90
@@ -12,19 +12,21 @@
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  class(Field_Type),      intent(inout) :: Flow  !! parent flow object
-  type(Grid_Type),        intent(in)    :: Grid  !! grid object
-  type(Var_Type), target                :: phi
-  real, optional, target, intent(out)   :: phi_x_opt(-Grid % n_bnd_cells  &
-                                                     :Grid % n_cells)
-  real, optional, target, intent(out)   :: phi_y_opt(-Grid % n_bnd_cells  &
-                                                     :Grid % n_cells)
-  real, optional, target, intent(out)   :: phi_z_opt(-Grid % n_bnd_cells  &
-                                                     :Grid % n_cells)
+  class(Field_Type),       intent(inout) :: Flow  !! parent flow object
+  type(Grid_Type), target, intent(in)    :: Grid  !! grid object
+  type(Var_Type),  target                :: phi
+  real, optional,  target, intent(out)   :: phi_x_opt(-Grid % n_bnd_cells  &
+                                                      :Grid % n_cells)
+  real, optional,  target, intent(out)   :: phi_y_opt(-Grid % n_bnd_cells  &
+                                                      :Grid % n_cells)
+  real, optional,  target, intent(out)   :: phi_z_opt(-Grid % n_bnd_cells  &
+                                                      :Grid % n_cells)
 !-----------------------------------[Locals]-----------------------------------!
-  integer       :: c, c1, c2, iter, s
+  integer       :: c, c1, c2, iter, s, i
   real          :: dx, dy, dz
   real, pointer :: phi_x(:), phi_y(:), phi_z(:)
+  real, pointer :: phi_i(:), xc_i(:)
+  real          :: di
 !==============================================================================!
 
   call Profiler % Start('Grad_Pressure_Obstacle')
@@ -63,123 +65,62 @@
   end do
   !$acc end parallel
 
-  !--------------------------------------!
-  !   Iterativelly improve x gradients   !
-  !--------------------------------------!
-  do iter = 1, 4
+  !------------------------------------------------------!
+  !                                                      !
+  !   Browse through all (three) coordinate directions   !
+  !                                                      !
+  !------------------------------------------------------!
+  do i = 1, 3
 
-    !$acc parallel loop independent
-    do s = 1, Grid % n_int_faces  ! loop through inter-faces
-      c1 = Grid % int_faces_c(1, s)
-      c2 = Grid % int_faces_c(2, s)
-      dx = Grid % xc(c2) - Grid % xc(c1)
-      ! Assert(Grid % fluid(c1) .ne. Grid % fluid(c2))
-      if(abs(dx) .gt. TINY) then
-        if(Grid % fluid(c1) .eq. 1) then
-          phi % n(c2) = phi % n(c1) + phi_x(c1) * dx
-        else
-          phi % n(c1) = phi % n(c2) - phi_x(c2) * dx
+    if(i .eq. 1) xc_i  => Grid % xc  ! alias to cell centers
+    if(i .eq. 1) phi_i => phi_x      ! alias to derivatives in i direction
+    if(i .eq. 2) xc_i  => Grid % yc  ! alias to cell centers
+    if(i .eq. 2) phi_i => phi_y      ! alias to derivatives in i direction
+    if(i .eq. 3) xc_i  => Grid % zc  ! alias to cell centers
+    if(i .eq. 3) phi_i => phi_z      ! alias to derivatives in i direction
+
+    !--------------------------------------!
+    !   Iterativelly improve x gradients   !
+    !--------------------------------------!
+    do iter = 1, 4
+
+      !$acc parallel loop independent
+      do s = 1, Grid % n_int_faces  ! loop through inter-faces
+        c1 = Grid % int_faces_c(1, s)
+        c2 = Grid % int_faces_c(2, s)
+        di = xc_i(c2) - xc_i(c1)
+        ! Assert(Grid % fluid(c1) .ne. Grid % fluid(c2))
+        if(abs(di) .gt. TINY) then
+          if(Grid % fluid(c1) .eq. 1) then
+            phi % n(c2) = phi % n(c1) + phi_i(c1) * di
+          else
+            phi % n(c1) = phi % n(c2) - phi_i(c2) * di
+          end if
         end if
-      end if
-    end do
-    !$acc end parallel
+      end do
+      !$acc end parallel
+
+      !$acc parallel loop independent
+      do c2 = -Grid % n_bnd_cells, -1
+        c1 = Grid % cells_c(1,c2)
+        di = xc_i(c2) - xc_i(c1)
+        phi % n(c2) = phi % n(c1) + phi_i(c1) * di
+      end do
+      !$acc end parallel
+
+      call Flow % Grad_Component(Grid, phi % n, i, phi_i)
+    end do  ! iter
 
     !$acc parallel loop independent
-    do c2 = -Grid % n_bnd_cells, -1
-      c1 = Grid % cells_c(1,c2)
-      dx = Grid % xc(c2) - Grid % xc(c1)
-      phi % n(c2) = phi % n(c1) + phi_x(c1) * dx
+    do c = 1, Grid % n_cells
+      if(Grid % fluid(c) .eq. 0) phi % n(c) = 0.0
+      if(Grid % fluid(c) .eq. 0) phi_i  (c) = 0.0
     end do
     !$acc end parallel
 
-    call Flow % Grad_Component(Grid, phi % n, 1, phi_x)
-  end do  ! iter
-
-  !$acc parallel loop independent
-  do c = 1, Grid % n_cells
-    if(Grid % fluid(c) .eq. 0) phi % n(c) = 0.0
-    if(Grid % fluid(c) .eq. 0) phi_x  (c) = 0.0
   end do
-  !$acc end parallel
 
-  !--------------------------------------!
-  !   Iterativelly improve y gradients   !
-  !--------------------------------------!
-  do iter = 1, 4
-
-    !$acc parallel loop independent
-    do s = 1, Grid % n_int_faces  ! loop through inter-faces
-      c1 = Grid % int_faces_c(1, s)
-      c2 = Grid % int_faces_c(2, s)
-      dy = Grid % yc(c2) - Grid % yc(c1)
-      ! Assert(Grid % fluid(c1) .ne. Grid % fluid(c2))
-      if(abs(dy) .gt. TINY) then
-        if(Grid % fluid(c1) .eq. 1) then
-          phi % n(c2) = phi % n(c1) + phi_y(c1) * dy
-        else
-          phi % n(c1) = phi % n(c2) - phi_y(c2) * dy
-        end if
-      end if
-    end do
-    !$acc end parallel
-
-    !$acc parallel loop independent
-    do c2 = -Grid % n_bnd_cells, -1
-      c1 = Grid % cells_c(1,c2)
-      dy = Grid % yc(c2) - Grid % yc(c1)
-      phi % n(c2) = phi % n(c1) + phi_y(c1) * dy
-    end do
-    !$acc end parallel
-
-    call Flow % Grad_Component(Grid, phi % n, 2, phi_y)
-  end do  ! iter
-
-  !$acc parallel loop independent
-  do c = 1, Grid % n_cells
-    if(Grid % fluid(c) .eq. 0) phi % n(c) = 0.0
-    if(Grid % fluid(c) .eq. 0) phi_y  (c) = 0.0
-  end do
-  !$acc end parallel
-
-  !--------------------------------------!
-  !   Iterativelly improve z gradients   !
-  !--------------------------------------!
-  do iter = 1, 4
-
-    !$acc parallel loop independent
-    do s = 1, Grid % n_int_faces  ! loop through inter-faces
-      c1 = Grid % int_faces_c(1, s)
-      c2 = Grid % int_faces_c(2, s)
-      dz = Grid % zc(c2) - Grid % zc(c1)
-      ! Assert(Grid % fluid(c1) .ne. Grid % fluid(c2))
-      if(abs(dz) .gt. TINY) then
-        if(Grid % fluid(c1) .eq. 1) then
-          phi % n(c2) = phi % n(c1) + phi_z(c1) * dz
-        else
-          phi % n(c1) = phi % n(c2) - phi_z(c2) * dz
-        end if
-      end if
-    end do
-    !$acc end parallel
-
-    !$acc parallel loop independent
-    do c2 = -Grid % n_bnd_cells, -1
-      c1 = Grid % cells_c(1,c2)
-      dz = Grid % zc(c2) - Grid % zc(c1)
-      phi % n(c2) = phi % n(c1) + phi_z(c1) * dz
-    end do
-    !$acc end parallel
-
-    call Flow % Grad_Component(Grid, phi % n, 3, phi_z)
-  end do  ! iter
-
-  !$acc parallel loop independent
-  do c = 1, Grid % n_cells
-    if(Grid % fluid(c) .eq. 0) phi % n(c) = 0.0
-    if(Grid % fluid(c) .eq. 0) phi_z(c)   = 0.0
-  end do
-  !$acc end parallel
-
+  ! This is probably not needed, anyhoo
   !$acc parallel loop independent
   do c = 1, Grid % n_cells
     if(Grid % fluid(c) .eq. 0) then
