726a48f98bfd3135b3378d46f2664c3bc2ad78dc
Niceno <bojan.niceno.scientist@gmail.com>
2024-03-03 18:35:12 +0100

    Moved basic info from Main.f90 to readme.md
    On branch main
    
    modified:   Evolutionary/Step_001/Main.f90
    new file:   Evolutionary/Step_001/readme.md
    modified:   Evolutionary/Step_002/Main.f90
    new file:   Evolutionary/Step_002/readme.md

--- Evolutionary/Step_002/Main.f90
+++ Evolutionary/Step_002/Main.f90
@@ -1,35 +1,7 @@
 !==============================================================================!
   program Main
 !------------------------------------------------------------------------------!
-!   In a nutshell, this program serves the same function as the previous step  !
-!   (stored in ../Step_001), which is to compute the sum of three matrices:    !
-!   c = a + b on a GPU.                                                        !
-!                                                                              !
-!   Although the first step to write a Fortran program and couple it with GPU  !
-!   functionality was a success, and although that first step entailed all the !
-!   functionality a typical CFD simulation on GPUs would entail (1 - creation  !
-!   of data on the "host"; 2 - copying the data to the "device"; 3- performing !
-!   the computations on the "device"; 4 - copying the computed data back to    !
-!   "host" and 5 - postprocessing the results on the "host", many questions    !
-!   remained open.                                                             !
-!                                                                              !
-!   Given that the first step had all the functionality within one source      !
-!   file, spanning a mere few dozen of lines, the qustions which arises next   !
-!   is how to embed this functionality within a bigger project?  How to deal   !
-!   with Fortran-GPU coupling inside a program which has many modules, a       !
-!   program whose data is passed from one module to another, a program which   !
-!   several hundreds of line of code?                                          !
-!                                                                              !
-!   Although these questions are mind-boggling, it is probably possible to     !
-!   address them one at a time.  This was the motivation to take this step.    !
-!   This step should have the same functionality as the first one, but it is   !
-!   supposed to be spread over several functions, global functions at this     !
-!   time.                                                                      !
-!                                                                              !
-!   What this step brings is the main function which creates the data on the   !
-!   "host" and makes the post-processing (still only printing), but copying to !
-!   "device", computations on "device" and copying the data back to the "host" !
-!   is performed by different global functions here.                           !
+!   Described in the readme.md file.  Compiled with the script compiled.sh.    !
 !------------------------------------------------------------------------------!
   implicit none
 !------------------------------------------------------------------------------!
@@ -44,11 +16,11 @@
   c = 0.0
 
   !  Copy data to "device", perform computations there, and get result back
-  call copy_to_device   (N, a)        ! operand in c = a + b, copy its contents
-  call copy_to_device   (N, b)        ! operand in c = a + b. copy its contents
-  call create_on_device (N, c)        ! result in c = a + b, create memory only
-  call compute_on_device(N, a, b, c)  ! compute on the "device"
-  call copy_from_device (N, c)        ! retrieve the result
+  call Copy_To_Device   (N, a)        ! operand in c = a + b, copy its contents
+  call Copy_To_Device   (N, b)        ! operand in c = a + b. copy its contents
+  call Create_On_Device (N, c)        ! result in c = a + b, create memory only
+  call Compute_On_Device(N, a, b, c)  ! compute on the "device"
+  call Copy_From_Device (N, c)        ! retrieve the result
 
   ! Print (post-process) the result from the "host"
   print *, 'Matrix c(1,1):', c(1,1)
@@ -57,14 +29,14 @@
   end program
 
 !==============================================================================!
-  subroutine copy_to_device(n, c)
+  subroutine Copy_To_Device(n, c)
 !------------------------------------------------------------------------------!
 !   This subroutines creates (allocates) memory on "device" for varible c, and !
 !   it also coppies its contents to the device.  It is useful for data which   !
 !   are operands (as opposed to results) in the computations performed on the  !
 !   "device".                                                                  !
 !                                                                              !
-!   Subroutine related to this one is "create_one_device" which allocates      !
+!   Subroutine related to this one is "Create_On_Device" which allocates       !
 !   memory  on the "device", but does not copy the contents to the "device".   !
 !------------------------------------------------------------------------------!
   implicit none
@@ -78,7 +50,7 @@
   end subroutine
 
 !==============================================================================!
-  subroutine create_on_device(n, c)
+  subroutine Create_On_Device(n, c)
 !------------------------------------------------------------------------------!
 !   This subroutines creates (allocates) memory on "device" for varible c, but !
 !   it doesn't copy its contents to the device.  It is useful for data which   !
@@ -86,7 +58,7 @@
 !   written by the calculations on the "device", the data which is the result  !
 !   of computations on the device.                                             !
 !                                                                              !
-!   Subroutine related to this one is "copy_to_device" which allocates memory  !
+!   Subroutine related to this one is "Copy_To_Device" which allocates memory  !
 !   on the "device", but also coppies the contents to the "device".            !
 !------------------------------------------------------------------------------!
   implicit none
@@ -100,7 +72,7 @@
   end subroutine
 
 !==============================================================================!
-  subroutine copy_from_device(n, c)
+  subroutine Copy_From_Device(n, c)
 !------------------------------------------------------------------------------!
 !   Explicitly retrieve data stored in c, from "device" to the "host".  This   !
 !   is useful for results obtained on the "device", which need further post-   !
@@ -117,7 +89,7 @@
   end subroutine
 
 !==============================================================================!
-  subroutine compute_on_device(n, a, b, c)
+  subroutine Compute_On_Device(n, a, b, c)
 !------------------------------------------------------------------------------!
 !   This subroutine performs computations on "device", on the data which was   !
 !   previously (see the main funciton) transferred to the "device".            !
@@ -126,7 +98,7 @@
 !   be coupled with "!$acc end data" for compiler to work, for which I belived !
 !   were just checks, to make sure that data is on the "device".  However, I   !
 !   have learned later that the combination of these two functions is actually !
-!   implicitly copying data back to host.  Hence, in order to improve perfor-  !
+!   physically copying data back to host.  Hence, in order to improve perfor-  !
 !   mance, maybe we should be more brave, and just leave these statements      !
 !   behind.  Indeed, try to compile the code without them to see that the code !
 !   works just the same.                                                       !
---
+++ Evolutionary/Step_002/readme.md
@@ -0,0 +1,20 @@
+# Introduction: Extending Functionality
+
+In a nutshell, this program serves the same function as the previous step (stored in `../Step_001`), which is to compute the sum of three matrices: `c = a + b` on a GPU.
+
+# Motivation: Beyond the Basics
+
+Although the first step to write a Fortran program and couple it with GPU functionality was a success, and although that first step entailed all the functionality a typical CFD simulation on GPUs would entail (1 - creation of data on the "host"; 2 - copying the data to the "device"; 3- performing the computations on the "device"; 4 - copying the computed data back to "host" and 5 - post-processing the results on the "host"), many questions remained open.
+
+# Expanding the Framework: Modularization Challenges
+
+Given that the first step had all the functionality within one source file spanning a mere couple of dozens of lines, the question that arises next is how to embed this functionality within a bigger project? How to deal with Fortran-GPU coupling inside a program that has many modules, a program whose data is passed from one module to another, a program that spans several hundreds or even thousands of lines of code?
+
+# Implementation: Splitting the Task
+
+What this step brings is the main function which creates the data on the "host" and makes the post-processing (still only printing), but copying to "device", computations on "device", and copying the data back to the "host" are performed by different global functions here.
+
+# Compilation and Execution
+
+The program is compiled with the script `compile.sh` and it requires Nvidia's Fortran compiler, which comes with Nvidia HPC-SDK. The script will create two executables: the `With_Gpu` which uses GPUs and `No_Gpu` which doesn't use GPUs.
+
